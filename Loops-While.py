6167632a7da57a4384c0e9a35a9ee0d46b9a0494
KatzeMatt10 <mateo.colmenares@udea.edu.co>
2022-01-31 22:02:50 -0500

    Fundamental loop - While 31/01/2022

---
+++ Loops-While.py
@@ -0,0 +1,13 @@
+def run():
+    Limit = input("Define the maximun number: ")
+    Limit = int(Limit)
+    Power = 0
+    i = 0
+    while (Power < Limit):
+        i += 1
+        Power = 2 ** (i)
+        print(f'2^{i}: {Power}') 
+
+
+if __name__ == "__main__": # Activation code
+    run()
